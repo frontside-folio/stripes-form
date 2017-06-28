a6b82e350bfd94ff4ec4f1ffe59a650811823489
Zak Burke <zburke@cornell.edu>
2017-06-28 15:28:23 -0400

    slay the eslint monster
    include own .eslintrc, CHANGELOG.md.
    clean up whitespace, commas, and semicolons.
    clean up imports and dependencies.
    refactor as stateless components.

--- StripesFormModal.js
+++ StripesFormModal.js
@@ -1,20 +1,27 @@
-import React, { Component, PropTypes } from 'react';
-import { isDirty } from 'redux-form';
-
+import React from 'react';
+import PropTypes from 'prop-types';
 import Modal from '@folio/stripes-components/lib/Modal';
 import Button from '@folio/stripes-components/lib/Button';
 import css from './StripesFormModal.css';
 
-export default class StripesFormModal extends Component {
-    render() {
-        return (
-            <Modal onClose={this.props.closeCB} size="small" open={this.props.openWhen} label="There are unsaved changes" dismissible>
-                <div className={css.stripesFormModal}>
-                    {this.props.remoteSave && <Button onClick={this.props.saveChanges} fullWidth>Save Changes</Button>}
-                    <Button onClick={this.props.discardChanges} fullWidth>Discard Changes</Button>
-                    <Button onClick={this.props.closeCB} buttonStyle="secondary" fullWidth>Cancel</Button>
-                </div>
-            </Modal>
-        ); 
-    };
+function StripesFormModal(props) {
+  return (
+    <Modal onClose={props.closeCB} size="small" open={props.openWhen} label="There are unsaved changes" dismissible>
+      <div className={css.stripesFormModal}>
+        {props.remoteSave && <Button onClick={props.saveChanges} fullWidth>Save Changes</Button>}
+        <Button onClick={props.discardChanges} fullWidth>Discard Changes</Button>
+        <Button onClick={props.closeCB} buttonStyle="secondary" fullWidth>Cancel</Button>
+      </div>
+    </Modal>
+  );
 }
+
+StripesFormModal.propTypes = {
+  openWhen: PropTypes.bool,
+  saveChanges: PropTypes.func,
+  discardChanges: PropTypes.func,
+  closeCB: PropTypes.func,
+  remoteSave: PropTypes.bool,
+};
+
+export default StripesFormModal;
