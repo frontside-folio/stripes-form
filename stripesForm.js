ea1d816024842c0644277c23559477889a60fc02
William Welling <wwelling@library.tamu.edu>
2017-12-14 08:59:17 -0600

    cleanup and lint fixes

--- stripesForm.js
+++ stripesForm.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { reduxForm, SubmissionError } from 'redux-form';
-import { connect } from '@folio/stripes-connect'; // eslint-disable-line
 import StripesFormWrapper from './StripesFormWrapper';
+
+const optWithOnSubmitFail = opts => Object.assign({}, opts, {
+  onSubmitFail: (errors, dispatch, submitError) => {
+    if (submitError && !(submitError instanceof SubmissionError)) {
+      console.error(submitError); // eslint-disable-line
+      throw new SubmissionError({ message: submitError.message });
+    } else {
+      console.warn(errors); // eslint-disable-line
+    }
+  },
+});
 
 export default function stripesForm(opts) {
   return (Form) => {
     const StripesForm = props => <StripesFormWrapper {...props} Form={Form} formOptions={opts} />;
-    opts = Object.assign({}, opts, {
-      onSubmitFail: (errors, dispatch, submitError) => {
-        if (submitError && !(submitError instanceof SubmissionError)) {
-          console.error(submitError); // eslint-disable-line
-          throw new SubmissionError({ message: submitError.message });
-        } else {
-          console.warn(errors); // eslint-disable-line
-        }
-      }
-    });
-    return reduxForm(opts)(StripesForm);
+    return reduxForm(optWithOnSubmitFail(opts))(StripesForm);
   };
 }
