a1632ea3758b74e60facdfb149b3953151cb5ecb
William Welling <wwelling@library.tamu.edu>
2017-12-14 08:07:00 -0600

    display errors on submit fail, rethrow when not a submission error

--- stripesForm.js
+++ stripesForm.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, SubmissionError } from 'redux-form';
 import { connect } from '@folio/stripes-connect'; // eslint-disable-line
 import StripesFormWrapper from './StripesFormWrapper';
 
 export default function stripesForm(opts) {
   return (Form) => {
-    const StripesForm = props => <StripesFormWrapper {...props} Form={Form} formOptions={opts} />;
+    const StripesForm = (props) => <StripesFormWrapper {...props} Form={Form} formOptions={opts} />;
+    opts.onSubmitFail = (errors, dispatch, submitError) => {
+      if (submitError && !(submitError instanceof SubmissionError)) {
+        console.error(submitError);
+        throw new SubmissionError({ message: submitError.message });
+      } else {
+        console.warn(errors);
+      }
+    };
     return reduxForm(opts)(StripesForm);
   };
 }
